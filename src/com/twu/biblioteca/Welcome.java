56a709c10aa75735df925cce0f0001aa7a95d1fe
GRXself <grxself@163.com>
2020-08-03 12:26:37 +0800

    [1.1]Add welcome message

--- src/com/twu/biblioteca/Welcome.java
+++ src/com/twu/biblioteca/Welcome.java
@@ -4,10 +4,10 @@ public class Welcome {
     private String welcomeMessage = "Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!";
 
     public String getWelcomeMessage(){
-        return "";
+        return welcomeMessage;
     }
 
     public void showWelcomeMessage(){
-
+        System.out.println(getWelcomeMessage());
     }
 }
