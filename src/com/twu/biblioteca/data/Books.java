c6b926a68808a6cdbe825de1ec3bc5a9540a5a30
GRXself <grxself@163.com>
2020-08-03 13:36:41 +0800

    [1.3]Change book model and books data, adding new propertys of author and year published.

--- src/com/twu/biblioteca/data/Books.java
+++ src/com/twu/biblioteca/data/Books.java
@@ -15,9 +15,15 @@ public class Books {
     private void generateBookList() {
         bookList = new ArrayList<Book>();
         char bookName = 'A';
+        char author = 'a';
+        int yearPublished = 1990;
         for (int i = 0; i < 10; i++) {
-            bookList.add(new Book(Character.toString(bookName)));
+            bookList.add(new Book(Character.toString(bookName),
+                    Character.toString(author),
+                    yearPublished));
             bookName += 1;
+            author += 1;
+            yearPublished += 1;
         }
     }
 
--- src/com/twu/biblioteca/model/Book.java
+++ src/com/twu/biblioteca/model/Book.java
@@ -2,16 +2,24 @@ package com.twu.biblioteca.model;
 
 public class Book {
     private String bookName;
+    private String author;
+    private int yearPublished;
 
-    public Book(String bookName){
+    public Book(String bookName, String author, int yearPublished){
         this.bookName = bookName;
+        this.author = author;
+        this.yearPublished = yearPublished;
     }
 
     public String getBookName() {
         return bookName;
     }
 
-    public void setBookName(String bookName) {
-        this.bookName = bookName;
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getYearPublished() {
+        return yearPublished;
     }
 }
