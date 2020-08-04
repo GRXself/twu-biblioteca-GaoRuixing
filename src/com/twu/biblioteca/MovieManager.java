cf98d5bd26170a52475b4394bcd24be5b0b71c28
GRXself <grxself@163.com>
2020-08-04 17:03:34 +0800

    [2.1]Complete basic movie manager functions.

--- src/com/twu/biblioteca/MovieManager.java
+++ src/com/twu/biblioteca/MovieManager.java
@@ -1,4 +1,47 @@
 package com.twu.biblioteca;
 
+import com.twu.biblioteca.data.Movies;
+import com.twu.biblioteca.model.Movie;
+
 public class MovieManager {
+    private Movies movies;
+
+    public MovieManager() {
+        movies = new Movies();
+    }
+
+    public void showMovies() {
+        System.out.println("Movie List");
+        movies.getMovieList().forEach(movie -> {
+            movieConsoleLinePrinter(movie);
+        });
+    }
+
+    private void movieConsoleLinePrinter(Movie movie) {
+        System.out.print(movie.getName());
+        StringBuilder nameWhiteSpace = new StringBuilder();
+        for (int i = movies.getMaxNameCharCount() - movie.getName().length() + 4; i > 0; i--) {
+            nameWhiteSpace.append(" ");
+        }
+        System.out.print(nameWhiteSpace);
+        System.out.print("|");
+
+        System.out.print(movie.getYear());
+        StringBuilder yearWhiteSpace = new StringBuilder();
+        for (int i = 2; i > 0; i--) {
+            yearWhiteSpace.append(" ");
+        }
+        System.out.print(yearWhiteSpace);
+        System.out.print("|");
+
+        System.out.print(movie.getDirector());
+        StringBuilder directorWhiteSpace = new StringBuilder();
+        for (int i = movies.getMaxDirectorCharCount() - movie.getDirector().length() + 2; i > 0; i--) {
+            directorWhiteSpace.append(" ");
+        }
+        System.out.print(directorWhiteSpace);
+        System.out.print("|");
+
+        System.out.println(movie.getRating());
+    }
 }
